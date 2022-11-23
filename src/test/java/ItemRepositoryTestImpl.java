458b72774f0ee33d696302188184a78fbd1ded41
Wongje4 <>
2022-11-23 18:34:07 +0800

    Initial Commit

--- src/test/java/ItemRepositoryTestImpl.java
+++ src/test/java/ItemRepositoryTestImpl.java
@@ -1,8 +1,5 @@
 import java.util.List;
 
-public class ItemRepositoryTestImpl implements ItemRepository{
-    @Override
-    public List<Item> findAll() {
-        return TestData.ALL_ITEMS;
-    }
+public interface ItemRepository {
+    List<Item> findAll();
 }
